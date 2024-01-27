f898c9d269cb41a4a975f2b6e3dae09371ecbc7d
tomerr90 <37086811+tomerr90@users.noreply.github.com>
2024-01-27 02:21:14 +0200

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,3 @@
 mvn clean package
-java --enable-preview -cp target/serde.jar org.example.FMASerDe > fmaSerDe
-java --enable-preview -cp target/serde.jar org.example.FMASerDeOffHeap > fmaSerDeOffHeap
+java --enable-preview -cp target/serde.jar org.example.FMASerDe
+java --enable-preview -cp target/serde.jar org.example.FMASerDeOffHeap
